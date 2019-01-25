c2846e2697e48346ae0e4af856ba3f44c24233c5
Johan Bichel Lindegaard <public@johan.cc>
2019-01-25 22:33:50 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js',
+    sourceMapFilename: 'js/[name].js.map',
+
+  },
+  module: {
+
+    
+    rules: [
+      { 
+        test: /src.*\.js$/, 
+        use: ['babel-loader'], 
+        exclude: /(node_modules|bower_components)/ 
+      },
+        
+      {
+        test: /\.(scss)$/,
+        use: [{
+          loader: 'style-loader', // inject CSS to page
+        }, {
+          loader: 'css-loader', // translates CSS into CommonJS modules
+        }, {
+          loader: 'postcss-loader', // Run post css actions
+          options: {
+            plugins: function () { // post css plugins, can be exported to postcss.config.js
+              return [
+                require('precss'),
+                require('autoprefixer')
+              ];
+            }
+          }
+        }, {
+          loader: 'sass-loader' // compiles Sass to CSS
+        }]
+      }
+      
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('css/bundle.css'),
+
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      inject: 'body'
+    })
+
+  ],
+
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  }
+  
+};
